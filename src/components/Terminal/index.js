67a3e63d6c4e82800575525c4e0f4ac90fbf620f
r1oga <r1oga@tuta.io>
2021-02-11 09:07:23 +0100

    Add css media query

--- src/components/Terminal/index.js
+++ src/components/Terminal/index.js
@@ -2,17 +2,21 @@ import ReactTerminal from 'react-terminal-component'
 import { ReactThemes } from 'react-terminal-component'
 
 import { emulatorState } from './emulatorState'
+import './Terminal.css'
 
 const Terminal = () => (
-  <ReactTerminal
-    promptSymbol="🐒"
-    emulatorState={emulatorState}
-    theme={{
-      ...ReactThemes.dye,
-      promptSymbolColor: '#f23f52',
-      commandColor: '#00a7e1'
-    }}
-  />
+  <div className="terminal">
+    <ReactTerminal
+      promptSymbol="🐒"
+      emulatorState={emulatorState}
+      theme={{
+        ...ReactThemes.dye,
+        promptSymbolColor: '#f23f52',
+        commandColor: '#00a7e1',
+        height: '60vh'
+      }}
+    />
+  </div>
 )
 
 export { Terminal }
